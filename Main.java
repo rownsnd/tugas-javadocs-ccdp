09df025e7871ec680a3d5eb9ca8ff17ecef38652
rownsnd <war11rior@gmail.com>
2025-11-09 16:14:21 +0700

    Perubahan minor

--- Main.java
+++ Main.java
@@ -1,16 +1,30 @@
+/**
+ * Kelas Main adalah entry point dari aplikasi manajemen gaji karyawan.
+ * @author Muhamad Haerul Anwar
+ * @version 1.0
+ */
 public class Main {
 
-   
     /**
-     * Contoh main method untuk menjalankan program.
+     * Metode utama (entry point) dari aplikasi.
+     * 
+     * @param args array argumen dari command line (tidak digunakan dalam program ini)
      */
     public static void main(String[] args) {
+        // Membuat objek Karyawan
         Karyawan karyawan = new Karyawan();
 
+        // Menetapkan data karyawan
         karyawan.setNama("Muhamad Haerul Anwar");
         karyawan.setGajiKotor(0);
+
+        // Menghitung gaji bersih
         karyawan.hitungPajak();
+
+        // Menghitung total gaji (bersih + tunjangan)
         karyawan.totalGajiKaryawan();
+
+        // Menampilkan data karyawan ke konsol
         karyawan.tampilGajiKaryawan();
     }
 }
